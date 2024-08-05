3bd617883f1117ffd00a942d71a413edfaf126ee
holden <james.holden.moore@gmail.com>
2024-08-05 14:16:01 -0400

    updates

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,7 @@ const initialCards = [
     title: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
   },
+
   {
     title: "Lake Louise",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
