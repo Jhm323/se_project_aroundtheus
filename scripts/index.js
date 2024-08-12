378766ed833c4b28cc480c87b12c44d27cc0bc21
holden <james.holden.moore@gmail.com>
2024-08-12 14:17:10 -0400

    updates

--- scripts/index.js
+++ scripts/index.js
@@ -29,3 +29,17 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+
+const profileEditBtn = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+
+profileEditBtn.addEventListener("click", () => {
+  profileEditModal.classList.add("modal__opened");
+});
+
+const modalCloseBtn = document.querySelector("#modal-close-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+
+modalCloseBtn.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal__opened");
+});
